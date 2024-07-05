d71de51e3604b14648e9ca82e6ede16e73aead85
AhmedHawasly <146901456+AhmedHawasly@users.noreply.github.com>
2024-07-05 15:53:45 +0900

    Final revision

--- hot_game.py
+++ hot_game.py
@@ -18,9 +18,9 @@ def main():
             tails_count += 1
     print(f"Heads: {heads_count}, Tails: {tails_count}")
     if heads_count > tails_count:
-        print("You won!")
+        print(f"{name} won!")
     else:
-        print("You lost!")
+        print(f"{name} lost!")
 
 if __name__ == "__main__":
     main()
