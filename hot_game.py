ee821469e5e35bc6ddd792b6c5aa807443e633a3
AhmedHawasly <146901456+AhmedHawasly@users.noreply.github.com>
2024-07-05 15:47:03 +0900

    Victory message added

--- hot_game.py
+++ hot_game.py
@@ -15,6 +15,10 @@ def main():
         else:
             tails_count += 1
     print(f"Heads: {heads_count}, Tails: {tails_count}")
+    if heads_count > tails_count:
+        print("You won!")
+    else:
+        print("You lost!")
 
 if __name__ == "__main__":
     main()
